e0134899f08a6fc6d4eb9e84e35190b66cc3a298
Dukuze <dudosart@Dominiques-MacBook-Pro.local>
2025-02-20 20:22:34 +0900

    Fix frontend page.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,26 @@
 import { title, subtitle } from "@/components/primitives";
 import DefaultLayout from "@/layouts/default";
-import HeartIcon from "@/components/home/home-media";
+import Resume from "@/components/home/home-media";
+import {Divider} from "@heroui/react";
 
 export default function IndexPage() {
   return (
     <DefaultLayout>
       <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
         <div className="inline-block max-w-xl text-center justify-center">
-          <span className={title()}>Make it &nbsp;</span>
-          <span className={title({ color: "violet" })}>beautiful&nbsp;</span>
-          <br />
-          <span className={title()}>
-            Welcome to dorna
+        <span className={title()}>
+            Welcome to Baka.me land<br />
           </span>
-          <div className={subtitle({ class: "mt-4" })}>
+          <span className={title()}>Land of &nbsp;</span>
+          <span className={title({ color: "violet" })}>Possibilities&nbsp;</span>
+          <br />
+          <div>
             Beautiful, fast and modern web development.
           </div>
           <div>
-          <HeartIcon/>
+          <Divider className="my-4" />
+          <span className={subtitle()}>Resume</span>
+          <Resume/>
           </div>
 
         </div>
